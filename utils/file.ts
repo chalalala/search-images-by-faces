159a02653c89f4cfb4b15dd8421b48321aa92d89
maidt <doantuyetmai2000@gmail.com>
2025-01-19 21:18:12 +0700

    feat: implement search photos by face

---
+++ utils/file.ts
@@ -0,0 +1,37 @@
+import { FaceRecognitionResult } from '@/types/faceRecognition';
+import JSZip from 'jszip';
+
+export const writeFile = (blobPart: BlobPart | undefined, fileName: string) => {
+  if (!blobPart) {
+    return;
+  }
+
+  const blob = new Blob([blobPart]);
+  const link = document.createElement('a');
+  const url = URL.createObjectURL(blob);
+
+  link.href = url;
+  link.download = fileName;
+  link.click();
+
+  URL.revokeObjectURL(url);
+};
+
+export const downloadAllImages = (results: FaceRecognitionResult[] | null) => {
+  if (!results) {
+    return;
+  }
+
+  const zip = new JSZip();
+  const img = zip.folder('images');
+
+  results.forEach((image) => {
+    img?.file(image.file.name, image.file, {
+      base64: true,
+    });
+  });
+
+  zip.generateAsync({ type: 'blob' }).then(function (content) {
+    writeFile(content, 'images.zip');
+  });
+};
